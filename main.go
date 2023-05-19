da8773e9f652ae14631d4ca69eb1e3292f0e5fd0
Daniel Chaffiol <daniel.chaffiol@caceis.com>
2023-05-19 20:12:55 +0200

    main.go: main feature extraction completed. Markdown to be improved

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ import (
 var cli struct {
 	EclipseVersion string `kong:"required,help='Eclipse version',alias='eversion',short='e'"`
 	TitleToMatch   string `kong:"required,help='Title or part of a title to match',alias='title',short='t'"`
+	PageToMatch    string `kong:"help='Page to match',alias='page',short='p'"`
+}
+
+type feature struct {
+	Title     string
+	Body      string
+	Category  string
+	PageTitle string
 }
 
 func main() {
@@ -25,16 +33,17 @@ func main() {
 	// Use the command line arguments
 	eclipseVersion := cli.EclipseVersion
 	titleToMatch := cli.TitleToMatch
+	pageToMatch := cli.PageToMatch
 
 	// URLs of the subPages
 	subPageURLs := []string{
 		"https://www.eclipse.org/eclipse/news/" + eclipseVersion + "/platform.php",
-		"https://www.eclipse.org/eclipse/news/" + eclipseVersion + "/java.php",
-		"https://www.eclipse.org/eclipse/news/" + eclipseVersion + "/pde.php",
 		"https://www.eclipse.org/eclipse/news/" + eclipseVersion + "/jdt.php",
+		"https://www.eclipse.org/eclipse/news/" + eclipseVersion + "/platform_isv.php",
+		"https://www.eclipse.org/eclipse/news/" + eclipseVersion + "/pde.php",
 	}
 
-	matchedTitles := make([]string, 0)
+	matchedFeatures := make([]feature, 0)
 
 	for _, url := range subPageURLs {
 		resp, err := http.Get(url)
@@ -52,65 +61,68 @@ func main() {
 			fmt.Println("Error loading HTML into goquery:", err)
 			continue
 		}
+		pageTitle := ""
+		currentCategory := ""
 
 		// Find the features
-		doc.Find("td.title").Each(func(i int, s *goquery.Selection) {
-			featureTitle := s.Text()
-			if strings.Contains(strings.ToLower(featureTitle), strings.ToLower(titleToMatch)) {
-				// Found a match, add it to the list
-				matchedTitles = append(matchedTitles, featureTitle)
+		doc.Find("h2,td.title,td.content").Each(func(i int, s *goquery.Selection) {
+			if s.Is("h2") {
+				if pageTitle == "" {
+					pageTitle = s.Text()
+					fmt.Printf("Processing page '%s'\n", pageTitle)
+				} else {
+					currentCategory = s.Text()
+					fmt.Printf("Found category '%s'\n", currentCategory)
+				}
+			} else if s.Is("td.title") {
+				featureTitle := s.Text()
+				fmt.Printf("Found feature title '%s'\n", featureTitle)
+				if strings.Contains(strings.ToLower(featureTitle), strings.ToLower(titleToMatch)) {
+					if strings.Contains(strings.ToLower(pageTitle), strings.ToLower(pageToMatch)) {
+						nextSibling := s.Next()
+						if nextSibling.Is("td.content") {
+							featureBody := nextSibling.Text()
+							converter := md.NewConverter("", true, nil)
+							markdown, _ := converter.ConvertString(featureBody)
+							matchedFeatures = append(matchedFeatures, feature{
+								Title:     featureTitle,
+								Body:      markdown,
+								Category:  currentCategory,
+								PageTitle: pageTitle,
+							})
+						}
+					}
+				}
 			}
 		})
 	}
 
-	if len(matchedTitles) > 1 {
+	if len(matchedFeatures) > 1 {
 		// If more than one title matched, just print the titles
-		for _, title := range matchedTitles {
-			fmt.Println(title)
+		for _, title := range matchedFeatures {
+			fmt.Printf("'%s - %s from '%s'\n", title.Title, title.Category, title.PageTitle)
 		}
-	} else if len(matchedTitles) == 1 {
+	} else if len(matchedFeatures) == 1 {
 		// If exactly one title matched, print the title and body
 		markdownResults := make([]string, 0)
-		for _, url := range subPageURLs {
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println("Error fetching URL:", err)
-				continue
-			}
-			defer resp.Body.Close()
+		feature := matchedFeatures[0]
+		body := feature.Body
 
-			bodyBytes, _ := io.ReadAll(resp.Body)
-			bodyString := string(bodyBytes)
+		// Convert to markdown
+		converter := md.NewConverter("", true, nil)
+		markdown, _ := converter.ConvertString(body)
 
-			doc, err := goquery.NewDocumentFromReader(strings.NewReader(bodyString))
-			if err != nil {
-				fmt.Println("Error loading HTML into goquery:", err)
-				continue
-			}
-
-			// Find the features again
-			doc.Find("td.title").Each(func(i int, s *goquery.Selection) {
-				featureTitle := s.Text()
-				if featureTitle == matchedTitles[0] {
-					// Found the match again, now get the body
-					body := s.Parent().Next().Find("td.content").Text()
-
-					// Convert to markdown
-					converter := md.NewConverter("", true, nil)
-					markdown, _ := converter.ConvertString(body)
-
-					// Print the title and body in markdown format
-					markdownResult := "> ## " + featureTitle + "\n"
-					for _, line := range strings.Split(markdown, "\n") {
-						markdownResult += "> " + line + "\n"
-					}
-					markdownResults = append(markdownResults, markdownResult)
-				}
-			})
+		// Print the title and body in markdown format
+		markdownResult := "> ## " + feature.Title + "\n"
+		for _, line := range strings.Split(markdown, "\n") {
+			markdownResult += "> " + line + "\n"
 		}
+		markdownResults = append(markdownResults, markdownResult)
 
 		// Copy the results to the clipboard
 		clipboard.WriteAll(strings.Join(markdownResults, "\n"))
 		fmt.Println("Results copied to clipboard.")
+	} else {
+		fmt.Println("No results found.")
 	}
 }
